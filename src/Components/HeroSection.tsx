d8352e06519ca7cd129e05e5639f31cd088f6a17
MuhammadMaaz66 <maaznaeem66@gmail.com>
2025-03-19 17:25:25 +0500

    hero section headers

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -8,12 +8,12 @@ function HeroSection() {
         <div className=''>
             <div className='md:w-[28%] sm:w-max-[10%] mx-5 mt-5 md:absolute md:top-[20%] md:justify-end md:left-[38%]'>
                 <img src={Brush} className='w-[10%] sm:w-max-[10%] h-[80px] object-contain justify-start' />
-                <p className='text-black font-gilroy font-semibold md:text-16px sm:text-14px'>Hair Styling</p>
+                <p className='text-black font-gilroy font-bold md:text-16px sm:text-14px'>Hair Styling</p>
                 <p className='text-black font-gilroy font-regular text-12 sm:text-10'>Professional Hair Stylists specializing in contemporary cuts, hair treatments, and personalized hair care.</p>
             </div>
             <div className=' sm:w-max-[10%] mx-5 mt-5 md:absolute md:top-[48%] md:left-[5%]'>
                 <img src={ScissorImage} className='w-[30%] sm:w-max-[10%] h-auto object-contain justify-start' />
-                <p className='text-black font-gilroy font-semibold md:text-16px sm:text-14px'>Hair Cut</p>
+                <p className='text-black font-gilroy font-bold md:text-16px sm:text-14px'>Hair Cut</p>
                 <p className='text-black font-gilroy font-regular text-12 sm:text-10 md:w-[60%] '>Skilled hair stylists offering precision cuts tailored to enhance individual style and personality.</p>
             </div>
             <div className='flex justify-end relative top-10 md:right-5'>
