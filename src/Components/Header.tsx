b72a87f5e45daee0eaed9614030c7e229d1a7ef5
MuhammadMaaz66 <maaznaeem66@gmail.com>
2025-03-19 17:14:38 +0500

    hero section

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,11 +9,11 @@ function Header() {
         <div className='flex items-center justify-between bg-black border-1 border-black w-full'>
             <img src={logo} alt="" className='w-full max-w-[150px] h-auto' />
             <div className='hidden md:flex items-center gap-10 mr-[5%]'>
-                <p className='text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2  hover:text-[#D33D75]'>HOME</p>
-                <p className='text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>About us</p>
-                <p className='text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Categories</p>
-                <p className='text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Specialists</p>
-                <p className='text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Special Offers</p>
+                <p className='font-gilroy font-bold text-white  md:text-[18px] hover:underline underline-offset-19 decoration-2  hover:text-[#D33D75]'>HOME</p>
+                <p className='font-gilroy text-white font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>About us</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Categories</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Specialists</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75]'>Special Offers</p>
             </div>
             <div className='md:hidden mr-[5%]'>
                 {!toggle ?
@@ -22,12 +22,12 @@ function Header() {
                     <IoMdClose onClick={() => setToggle(false)} />
                 } 
             </div>
-            <div className={`md:hidden ${toggle ? 'block' : 'hidden'} absolute top-[80px] left-0 right-0 bg-black py-5 text-center`}>
-                <p className='text-white font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2  hover:text-[#D33D75] cursor-pointer'>HOME</p>
-                <p className='text-white font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>About us</p>
-                <p className='text-white font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Categories</p>
-                <p className='text-white font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Specialists</p>
-                <p className='text-white font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Special Offers</p>
+            <div className={`md:hidden ${toggle ? 'block' : 'hidden'} absolute top-[80px] left-0 right-0 bg-black py-5 text-center z-1`}>
+                <p className='text-white font-gilroy font-bold   md:text-[18px] md:hover:underline underline-offset-19 decoration-2  hover:text-[#D33D75] cursor-pointer'>HOME</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>About us</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Categories</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Specialists</p>
+                <p className='text-white font-gilroy font-bold  md:text-[18px] md:hover:underline underline-offset-19 decoration-2 hover:text-[#D33D75] cursor-pointer'>Special Offers</p>
             </div>
         </div>
     )
--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header'
+import HeroSection from '../../Components/HeroSection'
 
 function LandingPage() {
     return (
         <div>
             <Header />
+            <HeroSection />
         </div>
     )
 }
